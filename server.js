a6de07825b58f83dd878ff2b4d2110af346a6d74
Kent C. Dodds <kent@doddsfamily.us>
2014-08-16 11:48:52 -0500

    Simplified backend app...

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 // general requires
 var express = require('express');
 var faker = require('faker');
-var path = require('path');
 
 // create the app
 var app = express();
@@ -12,8 +11,6 @@ var cors = require('cors');
 var expressJwt = require('express-jwt');
 var jwt = require('jsonwebtoken');
 var bodyParser = require('body-parser');
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 
 // database
 var user = {
@@ -29,41 +26,9 @@ app.use(cors());
 
 // setup jwt
 var jwtSecret = 'i3oifjda9302lr$#@05.';
-app.use(expressJwt({ secret: jwtSecret}).unless({path: ['/login']}));
+app.use(expressJwt({ secret: jwtSecret }).unless({path: ['/login']}));
 
-// setup passport
-passport.use(new LocalStrategy(function(username, password, done) {
-  if (username === user.username && password === user.password) {
-    done(null, user);
-  } else {
-    done(null, false, { message: 'Invalid username or password' });
-  }
-}));
-
-passport.serializeUser(function(user, done) {
-  done(null, user.username);
-});
-
-passport.deserializeUser(function(username, done) {
-  if (username === user.username) {
-    done(null, user);
-  } else {
-    done('No user with username ' + username);
-  }
-});
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/random-user', checkAuthenticated, function(req, res) {
-  var user = faker.Helpers.userCard();
-  user.avatar = faker.Image.avatar();
-  res.json(user);
-});
-
-app.post('/login', passport.authenticate('local'), function(req, res) {
+app.post('/login', authenticate, function(req, res) {
   var token = jwt.sign({
     username: req.user.username
   }, jwtSecret);
@@ -78,11 +43,29 @@ app.get('/me', checkAuthenticated, function(req, res) {
   res.json(req.user);
 });
 
+app.get('/random-user', checkAuthenticated, function(req, res) {
+  var user = faker.Helpers.userCard();
+  user.avatar = faker.Image.avatar();
+  res.json(user);
+});
+
 app.listen(3000, function() {
   console.log('server listening on :3000');
 });
 
 // UTIL FUNCTIONS
+
+function authenticate(req, res, next) {
+  var body = req.body;
+  if (!body.password || !body.username) {
+    return res.status(400).end('Must provide password and username');
+  }
+  if (body.username !== user.username || body.password !== user.password) {
+    return res.status(401).end('Username or password incorrect');
+  }
+  req.user = user;
+  next();
+}
 
 function checkAuthenticated(req, res, next) {
   if (req.user) {
