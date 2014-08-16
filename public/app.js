ed756c27462a284697bbf099426b5088c6405157
Kent C. Dodds <kent@doddsfamily.us>
2014-08-15 23:40:19 -0500

    Cookies get protected resource

--- public/app.js
+++ public/app.js
@@ -1,9 +1,77 @@
 (function() {
   'use strict';
   var app = angular.module('app', []);
-  app.controller('MainCtrl', function MainCtrl() {
+  app.controller('MainCtrl', function MainCtrl(UserFactory, RandomUserFactory) {
     'use strict';
     var vm = this;
-    vm.name = 'World';
+
+    // assignment
+    vm.login = login;
+    vm.logout = logout;
+    vm.getProtectedResource = getProtectedResource;
+
+    // initialization
+    UserFactory.getUser().then(function(response) {
+      vm.user = response.data;
+    });
+
+    // View Model functions
+    function login(username, password) {
+      UserFactory.login(username, password).then(function(response) {
+        vm.user = response.data;
+      }, handleError);
+    }
+
+    function logout() {
+      vm.user = null;
+      UserFactory.logout();
+    }
+
+    function getProtectedResource() {
+      RandomUserFactory.getRandomUser().then(function(response) {
+        vm.protectedResource = response.data;
+      }, handleError);
+    }
+
+
+    // UTIL FUNCTIONS
+    function handleError(response) {
+      alert('Error: ' + response.data);
+    }
+  });
+
+  app.factory('UserFactory', function LoginFactory($http) {
+    'use strict';
+    return {
+      getUser: getUser,
+      login: login,
+      logout: logout
+    };
+
+    function getUser() {
+      return $http.get('/me');
+    }
+
+    function login(username, password) {
+      return $http.post('/login', {
+        username: username,
+        password: password
+      });
+    }
+
+    function logout() {
+      return $http.get('/logout');
+    }
+  });
+
+  app.factory('RandomUserFactory', function RandomUserFactory($http) {
+    'use strict';
+    return {
+      getRandomUser: getRandomUser
+    };
+
+    function getRandomUser() {
+      return $http.get('/random-user');
+    }
   });
 })();
--- server.js
+++ server.js
@@ -1,17 +1,91 @@
+// general requires
 var express = require('express');
 var faker = require('faker');
 var path = require('path');
 
+// create the app
 var app = express();
 
-app.get('/random-user', function(req, res) {
+////////// authentication stuff
+// requires
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+// database
+var user = {
+  username: 'kentcdodds',
+  password: 'p'
+};
+
+// cookie and session stuff
+app.use(cookieParser());
+app.use(session({
+  name: 'sessionId',
+  secret: 'fjkdfeiow389024!',
+  saveUninitialized: true,
+  resave: true
+}));
+
+app.use(bodyParser.json());
+
+// setup passport
+passport.use(new LocalStrategy(function(username, password, done) {
+  if (username === user.username && password === user.password) {
+    done(null, user);
+  } else {
+    done(null, false, { message: 'Invalid username or password' });
+  }
+}));
+
+passport.serializeUser(function(user, done) {
+  done(null, user.username);
+});
+
+passport.deserializeUser(function(username, done) {
+  if (username === user.username) {
+    done(null, user);
+  } else {
+    done('No user with username ' + username);
+  }
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/random-user', checkAuthenticated, function(req, res) {
   var user = faker.Helpers.userCard();
   user.avatar = faker.Image.avatar();
   res.json(user);
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.post('/login', passport.authenticate('local'), function(req, res) {
+  res.json(req.user);
+});
+
+app.get('/logout', checkAuthenticated, function(req, res) {
+  req.logout();
+  res.json({ success: true });
+});
+
+app.get('/me', checkAuthenticated, function(req, res) {
+  res.json(req.user);
+});
 
 app.listen(3000, function() {
   console.log('server listening on :3000');
 });
+
+// UTIL FUNCTIONS
+
+function checkAuthenticated(req, res, next) {
+  if (req.user) {
+    next();
+  } else {
+    res.status(403).end('Must be authenticated');
+  }
+}
