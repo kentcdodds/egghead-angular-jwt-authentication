fc7c3e1cd8c3594bf002d8115d87eb87a6e6b3af
Kent C. Dodds <kent@doddsfamily.us>
2014-08-22 20:49:51 -0600

    Step 3.1 - Login on server, logging on client

--- public/app.js
+++ public/app.js
@@ -5,12 +5,20 @@
 
   app.constant('API_URL', 'http://localhost:3000');
 
-  app.controller('MainCtrl', function MainCtrl(RandomUserFactory) {
+  app.controller('MainCtrl', function MainCtrl(UserFactory, RandomUserFactory) {
     'use strict';
     var vm = this;
 
     // assignment
+    vm.login = login;
     vm.getRandomUser = getRandomUser;
+
+    // View Model functions
+    function login(username, password) {
+      UserFactory.login(username, password).then(function(response) {
+        console.log(response);
+      }, handleError);
+    }
 
     function getRandomUser() {
       RandomUserFactory.getRandomUser().then(function(response) {
@@ -33,6 +41,21 @@
 
     function getRandomUser() {
       return $http.get(API_URL + '/random-user');
+    }
+  });
+
+  app.factory('UserFactory', function LoginFactory($http, API_URL) {
+    'use strict';
+    return {
+      login: login
+    };
+
+    function login(username, password) {
+      var loginInfo = {
+        username: username,
+        password: password
+      };
+      return $http.post(API_URL + '/login', loginInfo);
     }
   });
 
--- server.js
+++ server.js
@@ -1,12 +1,43 @@
+// general requires
 var express = require('express');
 var faker = require('faker');
 
+// create the app
 var app = express();
 
+////////// authentication stuff
+// requires
 var cors = require('cors');
+var expressJwt = require('express-jwt');
+var jwt = require('jsonwebtoken');
+var bodyParser = require('body-parser');
+
+// database
+var user = {
+  username: 'kentcdodds',
+  password: 'p'
+};
+
+// parse json data coming from client (used on login)
+app.use(bodyParser.json());
 
 // setup cors
 app.use(cors());
+
+// setup jwt
+var jwtSecret = 'i3oifjda9302lr$#@05.';
+app.use(expressJwt({ secret: jwtSecret }).unless({path: ['/login']}));
+
+app.post('/login', authenticate, function(req, res) {
+  var token = jwt.sign({
+    username: req.user.username
+  }, jwtSecret);
+
+  res.json({
+    token: token,
+    user: user
+  });
+});
 
 app.get('/random-user', function(req, res) {
   var user = faker.Helpers.userCard();
@@ -17,3 +48,17 @@ app.get('/random-user', function(req, res) {
 app.listen(3000, function() {
   console.log('App listening on localhost:3000');
 });
+
+// UTIL FUNCTIONS
+
+function authenticate(req, res, next) {
+  var body = req.body;
+  if (!body.password || !body.username) {
+    return res.status(400).end('Must provide password and username');
+  }
+  if (body.username !== user.username || body.password !== user.password) {
+    return res.status(401).end('Username or password incorrect');
+  }
+  req.user = user;
+  next();
+}
